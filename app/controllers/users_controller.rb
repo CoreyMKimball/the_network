a4f10c0163a0d0f8b1f80a2736d8924249e6c120
CoreyMKimball <CoreyMKimball@gmail.com>
2014-01-12 20:49:26 -0800

    users_controller stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  
-  def show
-  	@user = User.find(params[:id])
-=======
 
   def show
     @user = User.find(params[:id])
->>>>>>> the_post
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 TheNetwork::Application.routes.draw do
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-resources :the_posts
-resources :users
-=======
  resources :users
->>>>>>> the_post
 
   root  'the_directory#home'
   match '/help',    to: 'the_directory#help',    via: 'get'
@@ -24,31 +18,6 @@ resources :users
   get "the_post/photography"
   get "the_post/pnd"
   get "the_post/dns"
-  
-<<<<<<< HEAD
-=======
- get "users/new"
- get "users/show"
->>>>>>> the_post
-
- get 'the_browse/music'
- get 'the_browse/film'
- get 'the_browse/literature'
- get 'the_browse/photography'
- get 'the_browse/pnd'
- get 'the_browse/dns'
-=======
-  resources :users
-  root 'the_directory#home'
-  get "users/new"
-  get "users/show"
-  get 'the_browse/music'
-  get 'the_browse/film'
-  get 'the_browse/literature'
-  get 'the_browse/photography'
-  get 'the_browse/pnd'
-  get 'the_browse/dns'
->>>>>>> user_models
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
